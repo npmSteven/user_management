d55465f5a5da3de948a69b3e675dea7ec6e159b7
Steven Rafferty <stevenrafferty2@protonmail.com>
2020-07-04 22:47:59 +0100

    Added routing with auth route

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -1,4 +1,4 @@
-import React, { useState, Dispatch } from 'react'
+import React, { Dispatch } from 'react'
 import { Button, Form } from 'semantic-ui-react'
 import { useSelector, useDispatch } from 'react-redux';
 import { login } from '../../utils/authentication';
@@ -7,6 +7,7 @@ import { Auth } from '../../models/Auth';
 import { AuthAction } from '../../models/AuthAction';
 import { SET_AUTH } from '../../actions/types';
 import { useFormInput } from '../hooks/customHooks';
+import { useHistory } from 'react-router-dom';
 
 export function Login() {
   const dispatch: Dispatch<AuthAction> = useDispatch();
@@ -14,13 +15,18 @@ export function Login() {
   const username = useFormInput('');
   const password = useFormInput('');
 
+  const history = useHistory();
+
   const auth: Auth = useSelector((state: any) => state.auth);
   
   const handleLogin = () => {
     const authAction: AuthAction = { type: SET_AUTH, auth: { status: 'loading' } };
     dispatch(authAction);
 
-    login(username.value, password.value, dispatch);
+    const isLoggedIn = login(username.value, password.value, dispatch);
+    if (isLoggedIn) {
+      history.push('/');
+    }
   };
 
   return (
