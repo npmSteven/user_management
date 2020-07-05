e1b0310e0978e670c47c598bde3a62218762b757
Steven Rafferty <stevenrafferty2@protonmail.com>
2020-07-05 21:28:20 +0100

    updated email validation

--- src/validation/userValidation.tsx
+++ src/validation/userValidation.tsx
@@ -3,5 +3,8 @@ import Joi from '@hapi/joi';
 export const userValidation = Joi.object({
   id: Joi.number(),
   name: Joi.string().min(2).max(32).required(),
-  email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required(),
+  email: Joi.string().email({
+    minDomainSegments: 2,
+    tlds: { allow: ['com', 'net', 'biz', 'tv', 'info', 'ca', 'org', 'me', 'io'] }
+  }).required(),
 });
